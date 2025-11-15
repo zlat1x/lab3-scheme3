71513005572c93f6c10448d5ac177001fd57cdfc
Zlata Dovbyk <dovbykzlata@gmail.com>
2025-11-15 23:05:06 +0200

    feat(pipeline): add worker_body and action tracing

--- scheme3.cpp
+++ scheme3.cpp
@@ -1,4 +1,6 @@
 #include "scheme3.h"
+#include <syncstream>
+#include <iostream>
 
 namespace scheme3 {
     pipeline::pipeline()
@@ -18,6 +20,47 @@ namespace scheme3 {
         int run = 1;
         for (int t = 0; t < NT; ++t) { s[t] = run; run +=q[t]; }
         return s;
+    }
+
+    void pipeline::do_action(char x, int i) {
+        std::osyncstream(std::cout) << "З набору " << x << " виконано дію " << i << ".\n";
+    }
+
+    void pipeline::worker_body(
+        int tid,
+        const std::vector<int>& qa, const std::vector<int>& sa,
+        const std::vector<int>& qb, const std::vector<int>& sb,
+        const std::vector<int>& qc, const std::vector<int>& sc,
+        const std::vector<int>& qd, const std::vector<int>& sd,
+        const std::vector<int>& qe, const std::vector<int>& se,
+        const std::vector<int>& qf, const std::vector<int>& sf,
+        const std::vector<int>& qg, const std::vector<int>& sg,
+        const std::vector<int>& qh, const std::vector<int>& sh,
+        const std::vector<int>& qi, const std::vector<int>& si,
+        const std::vector<int>& qj, const std::vector<int>& sj) 
+    {
+        for (int k = 0,  idx = sa[tid]; k < qa[tid]; ++k, ++idx) { do_action('a', idx); a_done.count_down(); }
+
+        a_done.wait();
+        for (int k = 0,  idx = sb[tid]; k < qb[tid]; ++k, ++idx) { do_action('b', idx); b_done.count_down(); }
+        for (int k = 0,  idx = sc[tid]; k < qc[tid]; ++k, ++idx) { do_action('c', idx); c_done.count_down(); }
+        for (int k = 0,  idx = sd[tid]; k < qd[tid]; ++k, ++idx) { do_action('d', idx); d_done.count_down(); }
+
+        b_done.wait();
+        for (int k = 0,  idx = se[tid]; k < qe[tid]; ++k, ++idx) { do_action('e', idx); e_done.count_down(); ef_done.count_down(); }
+
+        c_done.wait();
+        for (int k = 0,  idx = sf[tid]; k < qf[tid]; ++k, ++idx) { do_action('f', idx); f_done.count_down(); ef_done.count_down(); }
+        for (int k = 0,  idx = sg[tid]; k < qg[tid]; ++k, ++idx) { do_action('g', idx); g_done.count_down(); gh_done.count_down(); }
+
+        d_done.wait();
+        for (int k = 0,  idx = sh[tid]; k < qh[tid]; ++k, ++idx) { do_action('h', idx); h_done.count_down(); gh_done.count_down(); }
+
+        ef_done.wait();
+        for (int k = 0,  idx = si[tid]; k < qi[tid]; ++k, ++idx) { do_action('i', idx); i_done.count_down(); }
+
+        gh_done.wait();
+        for (int k = 0,  idx = sj[tid]; k < qj[tid]; ++k, ++idx) { do_action('j', idx); j_done.count_down(); }
     }
 
     void pipeline::run() {
--- scheme3.h
+++ scheme3.h
@@ -20,5 +20,19 @@ namespace scheme3 {
         private:
         static std::vector<int> split_even(int N);
         static std::vector<int> starts_from_quota(const std::vector<int>& q);
+
+        void worker_body(int tid,
+                         const std::vector<int>& qa, const std::vector<int>& sa,
+                         const std::vector<int>& qb, const std::vector<int>& sb,
+                         const std::vector<int>& qc, const std::vector<int>& sc,
+                         const std::vector<int>& qd, const std::vector<int>& sd,
+                         const std::vector<int>& qe, const std::vector<int>& se,
+                         const std::vector<int>& qf, const std::vector<int>& sf,
+                         const std::vector<int>& qg, const std::vector<int>& sg,
+                         const std::vector<int>& qh, const std::vector<int>& sh,
+                         const std::vector<int>& qi, const std::vector<int>& si,
+                         const std::vector<int>& qj, const std::vector<int>& sj);
+
+        static void do_action(char x, int i);
     };
 }
