a3a0ca9fdd5bd0a281d978d24849b815e4da643a
Zlata Dovbyk <dovbykzlata@gmail.com>
2025-11-15 22:18:27 +0200

    feat(pipeline): add constants and latch fields

--- scheme3.cpp
+++ scheme3.cpp
@@ -1,9 +1,13 @@
 #include "scheme3.h"
 
 namespace scheme3 {
-    pipeline::pipeline() = default;
+    pipeline::pipeline()
+        : a_done(A), b_done(B), c_done(C), d_done(D),
+          e_done(E), f_done(F), g_done(G), h_done(H),
+          i_done(I), j_done(J),
+          ef_done(E + F), gh_done(G + H) {}
 
     void pipeline::run() {
-        
+
     }
 }
--- scheme3.h
+++ scheme3.h
@@ -1,7 +1,18 @@
 #pragma once
 
+#include <latch>
+
 namespace scheme3 {
+    inline constexpr int NT = 5;
+    inline constexpr int A = 7, B = 6, C = 5, D = 6,
+                         E = 4, F = 7, G = 5, H = 5,
+                         I = 8, J = 5;
     struct pipeline {
+        std::latch a_done, b_done, c_done, d_done;
+        std::latch e_done, f_done, g_done, h_done;
+        std::latch i_done, j_done;
+        std::latch ef_done, gh_done;
+        
         pipeline();
         void run();
     };
